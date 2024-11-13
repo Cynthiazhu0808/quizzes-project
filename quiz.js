77534d6256e671d4eef07ae947b1522299a3a3a3
Cynthiazhu0808 <143716173+Cynthiazhu0808@users.noreply.github.com>
2024-11-12 19:40:02 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,21 @@
 //main javascript area
+<form quizform="form">
+        <p>    
+            <label for="wbur_button">WBUR 90.9 FM</label>
+            <input type="radio" name="station" value="wbur" id="wbur_button">
+        </p>
+        <p>
+            <label for="wbur_button">WBUR 90.9 FM</label>
+            <input type="radio" name="station" value="wbur" id="wbur_button">
+        </p>
+        <p>
+            <label for="wbur_button">WBUR 90.9 FM</label>
+            <input type="radio" name="station" value="wbur" id="wbur_button">
+        </p>
+        <p>
+            <label for="wbur_button">WBUR 90.9 FM</label>
+            <input type="radio" name="station" value="wbur" id="wbur_button">
+        </p>
+        <p><input type="button" value="Grade"></p>
+        <p><button type='button'>grade</button></p>
+    </form>
