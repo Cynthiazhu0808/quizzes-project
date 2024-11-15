fe46f58dc6ac8d16d6c4a9e69bc13720a2e9fb68
Cynthiazhu0808 <143716173+Cynthiazhu0808@users.noreply.github.com>
2024-11-15 14:02:13 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,26 +1,42 @@
-$(document).ready(function() {
+$(document).ready(() => {
+    // name the container with class 'quiz-questions' as container
     const container = $('.quiz-questions');
 
-    questions.forEach(function(questionData, index) {
+    questions.forEach((questionData, index) => {
         const question = new Question(questionData, index);
-        question.addToDOM(container);
-    });
-
-    // event handler for grading individual questions
-    container.on('click', '.grade-button', function() {
-        const questionDiv = $(this).closest('.question');
-        const selectedAnswer = questionDiv.find('input[type="radio"]:checked').val();
-
-        const correctAnswer = $(this).data('answer');
-
-        questionDiv.removeClass('correct wrong');
-
-        if (selectedAnswer == correctAnswer) {
-            questionDiv.addClass('correct');  // adds 'correct' class if answer is correct
-            $(this).text('Correct!');
-        } else {
-            questionDiv.addClass('wrong');  // adds 'wrong' class if answer is incorrect
-            $(this).text('Wrong!');
+        if (index===0 || index===1){
+            question.addToDOM($('#quiz1'));
+        }
+        else{
+            question.addToDOM($('#quiz2'));
         }
     });
+
+    container.one().on('click', '.grade-button', function(event) {  //when all .grade-button in container is clicked
+        const gradeButton = $(event.target);  //get the specific gradeButton that was clicked
+
+        const selectedAnswer = gradeButton.closest('.question').find('input[type="radio"]:checked').val();
+        //find the value of the checked radio button that is closest(in the parent) to gradeButton
+
+        // Get the correct answer stored in the data-answer attribute of the grade button
+        const correctAnswer = gradeButton.data('answer');
+
+        // Clear previous result classes before grading
+        questionDiv=gradeButton.closest('.question');
+        resulttext=questionDiv.find('.resultText');
+        questionDiv.removeClass('correct wrong');
+
+        // Check if the selected answer is correct
+        if (selectedAnswer === correctAnswer) {
+            // Mark as correct if the answer is right
+            questionDiv.addClass('correct');
+            resulttext.text("Correct!");
+        } else {
+            // Mark as wrong if the answer is incorrect
+            questionDiv.addClass('wrong');
+            resulttext.text("Wrong!");
+        }
+    });
+
+    
 });
