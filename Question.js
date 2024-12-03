0bbb178b1f041ac63bb7ff538a5e29eed9a329fc
Cynthiazhu0808 <143716173+Cynthiazhu0808@users.noreply.github.com>
2024-12-03 13:35:03 -0500

    Update Question.js

--- Question.js
+++ Question.js
@@ -1,42 +1,65 @@
-// Question.js
+'use strict';
 
 class Question {
-    constructor(questionData, questionNum) {
-        this.domElement = this.createQuestionDOM(questionData, questionNum);
-    }
+    element;
 
-    createQuestionDOM(questionData, questionNum) {
-        const questionDiv = $('<div>').addClass('question');
+    /** 
+    * Constructor
+    *
+    * @param {Object} questionData - a dictionary of the question content
+    * @param {number} questionNumber - a number of the question index
+    */
+    constructor(questionData, questionNumber) {
+        this.element=this.createDiv(questionData,questionNumber);
+    };
 
-        // Paragraph for the question text
-        const questionText = $('<p>').text(questionData.Q);
+    /** 
+    * A helper function which creates the div
+    *
+    * @param {Object} questionData - a dictionary of the question content
+    * @param {number} questionNumber - a number of the question index
+    * @return {Object} a div to be appended to the web page
+    */
+    createDiv(questionData,questionNumber){
+        let questionDiv = $('<div>').addClass('question'); //create a div with class question
+
+        let questionText = $('<p>').text("Q"+questionNumber+": "+questionData.Q);
         questionDiv.append(questionText);
 
-        // Create radio button options (A, B, C, D)
         ['A', 'B', 'C', 'D'].forEach(option => {
-            const label = $('<label>').text(questionData[option]);
+            // create a label with the text of each option
+            let label = $('<label>').text(questionData[option]);
 
-            const radioButton = $('<input>')
+            // create a radio button, with <input> tag, type as radio, and value set to be option
+            let radioButton = $('<input>')
                 .attr('type', 'radio')
-                .attr('name', `Q${questionNum}`) 
+                .attr('name', `Q${questionNumber}`) // unique name for each question
                 .val(option);
 
-            // Add radio button inside label, and then label to the question container
+            // Add radio button inside label, and then add label to the questionDiv
             label.prepend(radioButton);
-            questionDiv.append(label)
+            questionDiv.append(label).append('<br>');
         });
 
-        // grade button
-        const gradeButton = $('<button>')
+        // "Grade" button with data-answer attribute to store the correct answer
+        let gradeButton = $('<button>')
             .addClass('grade-button')
             .text('Grade')
             .attr('data-answer', questionData.ANS);
-        
-        questionDiv.append(gradeButton);
-        return questionDiv;
-    }
 
-    addToDOM(destination) {
-        $(destination).append(this.domElement);
-    }
+        // Append the grade button to the question container
+        questionDiv.append(gradeButton);
+        let resultText=$('<p>').addClass('resultText');
+        questionDiv.append(resultText);
+        return questionDiv;
+    };
+
+    /** 
+    * Method to add the question to a specified container on the page
+    *
+    * @param {Object} location - a jquery object of the location to append the question to 
+    */
+    addToDOM(location) {
+        location.append(this.element);
+    };
 }
